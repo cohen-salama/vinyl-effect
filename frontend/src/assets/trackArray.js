f167a59e0b60a67d116ef79c44ebecdb210f228b
sergiocohens <sergiocohensalama@pursuit.org>
2020-03-07 11:09:51 -0500

    better file structure

---
+++ frontend/src/assets/trackArray.js
@@ -0,0 +1,11 @@
+import track1 from './tracks/postal.mp3'
+import track2 from './tracks/x.mp3'
+import track3 from './tracks/inmersa.mp3'
+import track4 from './tracks/tematriste.mp3'
+import track5 from './tracks/elpresagio.mp3'
+import track6 from './tracks/algo.mp3'
+import track7 from './tracks/shanzhai.mp3'
+
+const trackArray = [track1,track2,track3,track4,track5,track6,track7]
+
+export default trackArray
